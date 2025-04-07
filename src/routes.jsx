e359a97b6561d0da9eb25648c211286758da60bf
sarahdti <sara.hedyati@gmail.com>
2025-04-07 14:54:46 +0330

    change rootes

--- src/routes.jsx
+++ src/routes.jsx
@@ -7,20 +7,30 @@ import Panel from "./assets/pages/panel/Panel"
 
 import PrivateRoute from "./assets/pages/PrivateRoute"
 
-const routes =[
-    {path:'/', element:<Home/>},
-    {path:'/about', element:<About/>},
-    {path:'/blog/', element: <Blog/> , children:[
-      {path:'yoga', element:<h4>یوگا</h4>},
-      {path:'workout', element:<h4>بدنسازی</h4>},
-      {path:'pilates', element:<h4>پیلاتس</h4>},
-    ]},
-    {path:'/course/:courseId', element:<Course/>},    
-    {path:'/login', element:<Login/>},
-    {path:'/panel', element:<PrivateRoute> <Panel/></PrivateRoute>},
-    
+// اگر Layoutی برای اشتراک هدر/فوتر داری می‌تونی اینجا ایمپورتش کنی
+// در غیر این صورت Home به عنوان Layout موقتی استفاده شده
 
+const routes = [
+  {
+    path: "/",
+    element: <Home />, // می‌تونی بجاش یه Layout کلی بذاری
+    children: [
+      { index: true, element: <Home /> }, // این مهم‌ترین بخشه برای روت اصلی
+      { path: "about", element: <About /> },
+      {
+        path: "blog",
+        element: <Blog />,
+        children: [
+          { path: "yoga", element: <h4>یوگا</h4> },
+          { path: "workout", element: <h4>بدنسازی</h4> },
+          { path: "pilates", element: <h4>پیلاتس</h4> },
+        ],
+      },
+      { path: "course/:courseId", element: <Course /> },
+      { path: "login", element: <Login /> },
+      { path: "panel", element: <PrivateRoute><Panel /></PrivateRoute> },
+    ],
+  },
+]
 
-  ]
-
-  export default routes;
+export default routes
