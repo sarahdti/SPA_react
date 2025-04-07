264e58e8bb5d31265d21f42021232f660f43ff26
sarahdti <sara.hedyati@gmail.com>
2025-04-07 14:36:49 +0330

    add basename

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
     <ThemeProvider dir="rtl">
-    <BrowserRouter>
+    <BrowserRouter basename="/SPA_react">
     <App/>
     </BrowserRouter>
   </ThemeProvider>
