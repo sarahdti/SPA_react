de5b211dc1e09447e9b6926f6c59b14cec606f6c
sarahdti <sara.hedyati@gmail.com>
2025-04-07 15:04:45 +0330

    add 404

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-   base: '/SPA_react/',
+  base: '/SPA_react/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: '/index.html'
+    }
+  }
 })
